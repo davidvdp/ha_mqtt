61583ccb2b16aa13e93449f9c5916ed6d75affc9
Davidvdp <davidvdp@gmail.com>
2020-04-03 23:45:32 +0200

    Added MANIFEST.in

--- setup.py
+++ setup.py
@@ -7,9 +7,16 @@ setup(
     name='dvdp.ha_mqtt',
     version='0.1.0',
     packages=['dvdp.ha_mqtt'],
-    url='',
+    url='https://github.com/davidvdp/ha_mqtt',
     author='David van der Pol',
-    description=open('README.md').read(),
+	license='MIT',
+	description='Home assistant mqtt devices interface'
+    long_description=open('README.md').read(),
     python_requires='>=3.7',
     install_requires=requirements,
+	include_package_data=True,
+	classifiers=[
+		'Programming Language :: Python :: 3'
+		'Programming Language :: Python :: 3.7'
+	],
 )
