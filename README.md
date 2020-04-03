a24d05e01a173447b6059f543c880f1aea9ee6fa
Davidvdp <davidvdp@gmail.com>
2020-04-04 00:51:03 +0200

    Pypi stuff

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ha_mqtt
-Home assistant mqtt devices interface
+# Home Assistant MQTT device interface
+Library that provides an interface for Home Assitant with a device using MQTT.
+
+Types of devices supported:
+1. Light
+
+## Install
+`$ pip install dvdp.ha_mqtt`
--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
 from distutils.core import setup
+from pathlib import Path
 
-with open('requirements.txt') as file:
+this_dir = Path(__file__).parent
+
+with open(this_dir / 'requirements.txt') as file:
     requirements = '\n'.join(file.readlines())
+
+with open(this_dir / 'README.md') as file:
+    long_description = file.read()
 
 setup(
     name='dvdp.ha_mqtt',
-    version='0.1.0',
+    version='0.2.0',
     packages=['dvdp.ha_mqtt'],
+    download_url='https://github.com/davidvdp/ha_mqtt/archive/v0.1.0.tar.gz',
     url='https://github.com/davidvdp/ha_mqtt',
     author='David van der Pol',
-	license='MIT',
-	description='Home assistant mqtt devices interface'
-    long_description=open('README.md').read(),
-    python_requires='>=3.7',
+    author_email='david@davidvanderpol.com',
+    license='MIT',
+    description='Home assistant mqtt devices interface',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=requirements,
-	include_package_data=True,
-	classifiers=[
-		'Programming Language :: Python :: 3'
-		'Programming Language :: Python :: 3.7'
-	],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords=[
+        'MQTT',
+        'Home',
+        'Assistant',
+    ],
 )
